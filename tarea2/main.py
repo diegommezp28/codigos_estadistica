890d580b7e5415801b0f5a8d3d6cb5e7b7dd502a
Diego <diego_gomezpolo@outlook.com>
2020-09-15 20:17:46 -0500

    Cambio de cosas"

--- tarea2/main.py
+++ tarea2/main.py
@@ -2,15 +2,13 @@ from scipy.stats import uniform, binom, norm
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-
 np.random.seed(seed=10)
 Y_list = []
 Z_list = []
-for i in range (0, 1000):
-    muestreo = uniform.rvs(loc=0, scale=1, size=1000)
+for i in range(0, 1000):
+    muestreo = uniform.rvs(loc=0, scale=1, size=100)
     media_muestral = np.mean(muestreo)
-    Y_n = (1001/1000) * np.amax(muestreo)
+    Y_n = (101 / 100) * np.amax(muestreo)
     Z_n = 2 * media_muestral
 
     Y_list.append(Y_n)
@@ -24,13 +22,13 @@ mu_Z = np.mean(Z_array)
 fig = plt.figure()
 
 plot_y = plt.hist(Y_array, 30, alpha=0.1, density=True, color='b')
-plt.axvline(x=np.mean(Y_array), color='b', lw=1, label= f' $Y_n$ sample $\mu = {round(mu_Y, 6)}$')
+plt.axvline(x=np.mean(Y_array), color='b', lw=1, label=f' $Y_n$ sample $\mu = {round(mu_Y, 6)}$')
 plot_Z = plt.hist(Z_array, 30, alpha=0.1, color='r', density=True)
-plt.axvline(x=np.mean(Z_array), color='r', lw=1, label= f'$Z_n$ sample $\mu = {round(mu_Z, 6)}$')
+plt.axvline(x=np.mean(Z_array), color='r', lw=1, label=f'$Z_n$ sample $\mu = {round(mu_Z, 6)}$')
 
 plt.legend()
-plt.title('1000 Samples')
+plt.title('100 Samples')
 
 plt.show()
 
-fig.savefig('1000 SAMPLES.pdf', bbox_inches='tight')
+fig.savefig('100 SAMPLES.png', bbox_inches='tight')
